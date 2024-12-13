2aeeb7e7ab32fc9ec365ab505e562aae42217d5c
lentonio <tom.lenton@uplearn.co.uk>
2024-12-13 11:27:34 +0000

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -70,11 +70,14 @@ with st.sidebar:
         yminordivisor = 1
 
     """# Image size"""
-    col7, col8 = st.columns(2)
+    col7, col8, col9 = st.columns(2)
     with col7:
         imageheight = st.number_input("Height", value=10)
     with col8:
         imagewidth = st.number_input("Width", value=10)
+    with col9:
+        unused = st.segmented_control("Gridlines", options = ["1", "π"], default = '1')
+        
 
 x_init = np.linspace(xlower, xupper, 100000)
 y_init = np.zeros_like(x_init)  # Create corresponding y values

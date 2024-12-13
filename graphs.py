0fb32f7e506a585405d152e5a9aef6409432f53d
lentonio <tom.lenton@uplearn.co.uk>
2024-12-13 09:15:17 +0000

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -58,19 +58,20 @@ yupper = yuserupper + 0.025 * ydifference
 showvalues = st.checkbox("Show values on axes", value=True)
 showticks = st.checkbox("Show ticks on axes")
 
-col5, col6 = st.columns(2)
-with col5:
-    x_base_step = st.number_input("x-axis step:", value=2)
-with col6:
-    x_is_pi = st.checkbox("Multiply x-step by π")
-xstep = x_base_step * (np.pi if x_is_pi else 1)
-
-col7, col8 = st.columns(2)
-with col7:
-    y_base_step = st.number_input("y-axis step:", value=2)
-with col8:
-    y_is_pi = st.checkbox("Multiply y-step by π")
-ystep = y_base_step * (np.pi if y_is_pi else 1)
+if showvalues or showticks:
+    col5, col6 = st.columns(2)
+    with col5:
+        x_base_step = st.number_input("x-axis step:", value=2)
+    with col6:
+        x_is_pi = st.checkbox("Multiply x-step by π")
+    xstep = x_base_step * (np.pi if x_is_pi else 1)
+    
+    col7, col8 = st.columns(2)
+    with col7:
+        y_base_step = st.number_input("y-axis step:", value=2)
+    with col8:
+        y_is_pi = st.checkbox("Multiply y-step by π")
+    ystep = y_base_step * (np.pi if y_is_pi else 1)
 
 # Grid style
 gridstyle = 'minor'  # Set as 'none', 'major', or 'minor'
